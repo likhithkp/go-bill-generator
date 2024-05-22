0a6a50bbbb16e49b99bf0577a23b804c0a204384
likhithkp <likhithkpdev@gmail.com>
2024-05-22 07:24:50 +0530

    Push

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello")
+	a := []string{}
+	a = append(a, "Likhith", "Shiba", "Shibashanth")
+	rangeOne := a[1:2]
+	fmt.Println(a)
+	fmt.Println(rangeOne)
 }
