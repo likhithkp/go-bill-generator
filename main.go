a6c5e664a5de9cb1ac5be5d8fef563ffb40e1cc9
likhithkp <likhithkpdev@gmail.com>
2024-05-23 07:27:34 +0530

    Split func

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"strings"
 )
 
+func getFirstLettersInCaps(n string) (string, string) {
+	arr := strings.Split(n, " ")
+
+	var names []string
+
+	for _, value := range arr {
+		names = append(names, value[:1])
+	}
+
+	if len(names) > 1 {
+		return strings.ToUpper(names[0]), strings.ToUpper(names[1])
+	}
+
+	return names[0], "_"
+}
+
 func main() {
-	a := []int{10, 20, 45, 55, 67, 85, 48, 95, 0, 12, 54}
-	index := sort.SearchInts(a, 67)
-	fmt.Println(index)
+	a, b := getFirstLettersInCaps("likhith kp")
+	c, d := getFirstLettersInCaps("shiba")
+
+	fmt.Println(a, b)
+	fmt.Println(c, d)
 }
