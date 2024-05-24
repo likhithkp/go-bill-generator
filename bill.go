7870974a6274672088b856c535fd1aadd226e3fc
likhithkp <likhithkpdev@gmail.com>
2024-05-24 09:40:17 +0530

    Get input and buffio

--- bill.go
+++ bill.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 type bill struct {
-	name string
-	bill map[string]float64
-	tip  float64
+	name  string
+	items map[string]float64
+	tip   float64
 }
 
 func newBill(name string) bill {
 	b := bill{
-		name: name,
-		bill: map[string]float64{"pie": 2.99, "cheeseCake": 3.99},
-		tip:  0,
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
 	}
 
 	return b
@@ -22,11 +22,20 @@ func (b bill) format() string {
 	fs := "Bill breakdown: \n"
 	var total float64 = 0
 
-	for k, v := range b.bill {
+	for k, v := range b.items {
 		fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
 		total += v
 	}
 
-	fs += fmt.Sprintf("%-25v ...$%0.2f", "total :", total)
+	fs += fmt.Sprintf("%-25v ...$%0.2f\n", "tip :", b.tip)
+	fs += fmt.Sprintf("%-25v ...$%0.2f\n", "total :", total+b.tip)
 	return fs
+}
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
